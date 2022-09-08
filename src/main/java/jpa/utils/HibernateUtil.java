88a2db051bedd147aa89a7eed9b01b9b4d18a02c
tomcruzana <tomcruzana@ymail.com>
2022-09-08 00:38:47 -0700

    created hibernate session factory singleton

---
+++ src/main/java/jpa/utils/HibernateUtil.java
@@ -0,0 +1,36 @@
+package jpa.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+	private static SessionFactory factory;
+	private static StandardServiceRegistry registry;
+
+	public static synchronized Session getSessionFactory() {
+		if (factory == null) {
+			final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+					.configure("hibernate.cfg.xml").build();
+			try {
+				factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+			} catch (Exception e) {
+				e.printStackTrace();
+				if (registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
+		}
+		return factory.openSession();
+	}
+
+	public static void shutdown() {
+		if (registry != null && factory != null) {
+			factory.close();
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+}
